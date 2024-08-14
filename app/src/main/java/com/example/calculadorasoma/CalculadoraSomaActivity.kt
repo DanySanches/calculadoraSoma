868f38ae12918dafe72e4ea437bc2f8c50cf78cd
DanySanches <danysanches@msn.com>
2024-08-14 10:05:00 -0300

    first

---
+++ app/src/main/java/com/example/calculadorasoma/CalculadoraSomaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.calculadorasoma
+
+import android.app.Activity
+import android.os.Bundle
+
+class CalculadoraSomaActivity : Activity() {
+    override fun onCreate(bundle: Bundle?){
+        super.onCreate(bundle)
+        setContentView(R.layout.calculadora_layout)
+    }
+}
