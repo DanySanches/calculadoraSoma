6726f1a812d01aba717adad596a40dde6eb40ff2
DanySanches <danysanches@msn.com>
2024-08-14 10:51:19 -0300

    ciclo de vida OnCreate(), OnStart(), onResume(), onPause(), onStop(), onDestroy().

--- app/src/main/java/com/example/calculadorasoma/CalculadoraSomaActivity.kt
+++ app/src/main/java/com/example/calculadorasoma/CalculadoraSomaActivity.kt
@@ -2,10 +2,35 @@ package com.example.calculadorasoma
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 
 class CalculadoraSomaActivity : Activity() {
     override fun onCreate(bundle: Bundle?){
         super.onCreate(bundle)
+        Log.v("CALCULADORA", "Activity criada onCreate()")
         setContentView(R.layout.calculadora_layout)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v("CALCULADORA", "Activity iniciada onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v("CALCULADORA", "Activity visivel onResume()")
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.v("CALCULADORA", "Activity invisivel onPause()")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.v("CALCULADORA", "Activity parada onStop()")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.v("CALCULADORA", "Activity  destruida onDestroy()")
     }
 }
